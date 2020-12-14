aacdd4677896fecf078598e472b9693715fb00f2
achantv <vamsee_achanta@oxy.com>
2020-12-14 12:14:06 -0600

    simple excel reading

---
+++ read_excel_example.py
@@ -0,0 +1,9 @@
+def read_excel(cfg):
+    from common.data import ReadData
+    read_data = ReadData()
+    data_frame = read_data.from_xlsx(cfg)
+    print(data_frame)
+
+if __name__ == '__main__':
+    cfg = {'io': 'data_manager/data/sample_data.xlsx'}
+    read_excel(cfg)
