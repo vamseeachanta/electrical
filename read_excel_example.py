88aee6a5e38b71e8b50da604baadff2785c15c7c
achantv <vamsee_achanta@oxy.com>
2020-12-14 12:43:16 -0600

    yapf formatting added

--- read_excel_example.py
+++ read_excel_example.py
@@ -1,8 +1,9 @@
 def read_excel(cfg):
     from common.data import ReadFromExcel
-    read_data =ReadFromExcel()
+    read_data = ReadFromExcel()
     data_frame = read_data.from_xlsx(cfg)
     print(data_frame)
+
 
 if __name__ == '__main__':
     cfg = {'io': 'data_manager/data/sample_data.xlsx'}
